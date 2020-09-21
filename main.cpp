79fb760374fe364f58593a9ecb4343fa3187e301
luciana1faur <luciana_faur@yahoo.com>
2020-09-21 12:04:47 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int contor;
+  contor = 1;
+ 
+  while(contor <= 4){
+    cout << "Contorul este: " << contor << endl;
+    contor++;
+    }
+  cout << "La revedere!";
+  
+  return 0;
+}
